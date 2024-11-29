808052fd27f93bc03d112fbac4e47b2da6d88d26
msheila1 <msheilacarneiro@gmail.com>
2024-11-29 20:58:58 +1030

    done project

--- src/app/models/order.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Order {
-    name: string;
-    email: string;
-    phone: string;
-    address: string;
-    // items: Product[];
-  }
-  
--- src/app/models/product.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Product {
-    id: number;
-    title: string;
-    price: number;
-    description: string;
-    image: string;
-    items: Product[];
-  }
-  
